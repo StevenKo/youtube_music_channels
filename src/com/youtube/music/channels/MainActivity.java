836ff32d33fbdbb60bcdf006fd559c73b8198de6
Steven Ko <chunyuko85@gmaill.com>
2013-01-23 21:48:47 +0800

    加入server api, 拿掉youtube lib

--- src/com/youtube/music/channels/MainActivity.java
+++ src/com/youtube/music/channels/MainActivity.java
@@ -1,35 +1,32 @@
 package com.youtube.music.channels;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.ArrayList;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 
-import com.google.gdata.client.youtube.YouTubeQuery;
+import com.youtube.music.channels.api.ChannelApi;
+import com.youtube.music.channels.entity.Channel;
 
 public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        new AsyncTask() {
+
+            @Override
+            protected Object doInBackground(Object... params) {
+                ArrayList<Channel> news = ChannelApi.getPromotionNews(1);
+                return null;
+            }
+
+        }.execute();
+
         setContentView(R.layout.activity_main);
-        YouTubeQuery query;
-        try {
-            query = new YouTubeQuery(new URL("http://gdata.youtube.com/feeds/api/videos"));
-            // order results by the number of views (most viewed first)
-            query.setOrderBy(YouTubeQuery.OrderBy.VIEW_COUNT);
 
-            // search for puppies and include restricted content in the search results
-            query.setFullTextQuery("puppy");
-            query.setSafeSearch(YouTubeQuery.SafeSearch.NONE);
-        } catch (MalformedURLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        // VideoFeed videoFeed = service.query(query, VideoFeed.class);
     }
 
     @Override
